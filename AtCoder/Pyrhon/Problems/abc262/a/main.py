e49baee11e219b2c7101f71519a9cf6113a6e14f
imk0512 <imk3626@gmail.com>
2022-12-13 23:42:51 +0900

    Add: 開発環境整い

---
+++ AtCoder/Pyrhon/Problems/abc262/a/main.py
@@ -0,0 +1,28 @@
+# import math
+# from collections import defaultdict
+
+
+# 文字列１個
+# N = input()
+# 数値 1個
+num = int(input())
+# 数値２個
+# N,S = map(int,input().split())
+# 数値3個
+# N,X,Y = map(int,input().split())
+
+# リスト　文字列
+# strList = list(map(input().split()))
+# リスト　数値
+# numList = list(map(int,input().split()))
+
+
+if num % 4 == 2:
+    print(num)
+else:
+    a = 4 - (num % 4)
+    print(num + a)
+
+
+def foo():
+    print("Hello" "World")
